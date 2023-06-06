86cdb02ae7d075acddc371ca795ec432480d45b9
issac.yi <issac.yi@corp.to8to.com>
2023-06-06 16:26:36 +0800

    新添加入库方式的选择入口

--- ImportLibraries.py
+++ ImportLibraries.py
@@ -28,12 +28,31 @@ def add_library(title, description, package_names):
 # 初始化CSV文件
 initialize_csv_file()
 
-# 引导用户录入三方库信息
-title = input("请输入库名称：")
-description = input("请输入库描述：")
-package_names = input("请输入库包名（以逗号分隔）：").split(',')
+# 让用户选择输入方式
+while True:
+    choice = input("请选择输入方式（1: 手动录入, 2: 补齐现有数据，输入 q 退出）：")
 
-# 添加库信息到CSV文件
-add_library(title, description, package_names)
+    if choice == "1":
+        while True:
+            # 引导用户手动录入三方库信息
+            title = input("请输入库名称：")
+            description = input("请输入库描述：")
+            package_names = input("请输入库包名（以逗号分隔）：").split(',')
 
-print("库信息已保存到 sdk_libraries.csv 文件中。")
+            # 添加库信息到CSV文件
+            add_library(title, description, package_names)
+
+            continue_input = input("继续录入下一个库信息？（输入 q 停止录入，按回车继续录入）：")
+            if continue_input.lower() == 'q':
+                break
+
+        print("库信息已保存到 sdk_libraries.csv 文件中。")
+    elif choice == "2":
+        # TODO: 根据现有数据补齐三方库信息的逻辑
+        print("补齐现有数据的逻辑尚未实现。")
+    elif choice.lower() == 'q':
+        break
+    else:
+        print("无效的选择，请重新选择输入方式。")
+
+print("程序已退出。")
