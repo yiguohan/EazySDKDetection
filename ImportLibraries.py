52417b7ad00de30309cf79f9b761b5e618e47bd0
issac.yi <issac.yi@corp.to8to.com>
2023-06-14 10:03:40 +0800

    添加数据修复功能

--- ImportLibraries.py
+++ ImportLibraries.py
@@ -2,57 +2,110 @@ import csv
 import os
 import uuid
 
-# 获取当前脚本所在的路径
-script_path = os.path.dirname(os.path.abspath(__file__))
-
-# 创建 data 文件夹
-csv_folder_path = os.path.join(script_path, "data")
-os.makedirs(csv_folder_path, exist_ok=True)
-
-# CSV 文件路径
-csv_file_path = os.path.join(csv_folder_path, "sdk_libraries.csv")
+csv_file_path = os.path.join("data", "sdk_libraries.csv")
 
 def initialize_csv_file():
     if not os.path.isfile(csv_file_path):
         with open(csv_file_path, 'w', newline='') as csv_file:
-            fieldnames = ['id', 'title', 'description', 'package_name']
+            fieldnames = ['id', 'title', 'description', 'package_names']
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
 
-def add_library(title, description, package_names):
-    library_id = str(uuid.uuid4())  # 生成唯一的库ID
+def add_library(library_data):
     with open(csv_file_path, 'a', newline='') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow([library_id, title, description, ','.join(package_names)])
+        writer.writerow(library_data)
+
+def generate_library_id():
+    return str(uuid.uuid4())
 
 # 初始化CSV文件
 initialize_csv_file()
 
-# 让用户选择输入方式
+# 引导用户选择修复方式
 while True:
-    choice = input("请选择输入方式（1: 手动录入, 2: 补齐现有数据，输入 q 退出）：")
-
-    if choice == "1":
+    input_option = input("请选择修复方式（1 - 手动修复，2 - 自动修复）：")
+    if input_option == "1":
         while True:
-            # 引导用户手动录入三方库信息
-            title = input("请输入库名称：")
-            description = input("请输入库描述：")
-            package_names = input("请输入库包名（以逗号分隔）：").split(',')
+            # 手动修复库信息
+            library_data = {}
+
+            library_data['id'] = generate_library_id()
+            library_data['title'] = input("请输入库名称：")
+            library_data['description'] = input("请输入库描述：")
+            package_names_input = input("请输入库包名（以逗号分隔）：")
+            library_data['package_names'] = package_names_input.split(',')
 
             # 添加库信息到CSV文件
-            add_library(title, description, package_names)
+            add_library(library_data)
 
-            continue_input = input("继续录入下一个库信息？（输入 q 停止录入，按回车继续录入）：")
-            if continue_input.lower() == 'q':
+            continue_input = input("继续修复下一个库信息？（输入 q 停止修复）：")
+            if continue_input.lower() == "q":
                 break
 
-        print("库信息已保存到 sdk_libraries.csv 文件中。")
-    elif choice == "2":
-        # TODO: 根据现有数据补齐三方库信息的逻辑
-        print("补齐现有数据的逻辑尚未实现。")
-    elif choice.lower() == 'q':
+        print("库信息已修复完成并保存到 sdk_libraries.csv 文件中。")
         break
-    else:
-        print("无效的选择，请重新选择输入方式。")
 
-print("程序已退出。")
+    elif input_option == "2":
+        # 自动修复库信息
+        existing_libraries = {}
+        with open(csv_file_path, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                package_names = row['package_names'].split(',')
+                existing_libraries[package_names[0]] = {
+                    'id': row['id'],
+                    'title': row['title'],
+                    'description': row['description'],
+                    'package_names': package_names
+                }
+
+        sdk_references_file = os.path.join("data", "sdk_references.csv")
+        if not os.path.isfile(sdk_references_file):
+            print("找不到 sdk_references.csv 文件，请先生成该文件。")
+            continue
+
+        with open(sdk_references_file, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                package_name = row['Package Name']
+                if package_name not in existing_libraries:
+                    print(f"包名：{package_name} 缺少字段信息，请修复。")
+
+                    library_data = {}
+                    library_data['id'] = generate_library_id()
+                    library_data['title'] = input(f"请输入库名称（{package_name}）：")
+                    library_data['description'] = input(f"请输入库描述（{package_name}）：")
+                    library_data['package_names'] = [package_name]
+
+                    # 添加库信息到CSV文件
+                    add_library(library_data)
+
+                else:
+                    existing_library = existing_libraries[package_name]
+                    missing_fields = []
+                    for field in ['title', 'description', 'package_names']:
+                        if field not in existing_library or not existing_library[field]:
+                            missing_fields.append(field)
+
+                    if missing_fields:
+                        print(f"包名：{package_name} 缺少字段信息，请修复。")
+
+                        for field in missing_fields:
+                            if field == 'title':
+                                existing_library['title'] = input(f"请输入库名称（{package_name}）：")
+                            elif field == 'description':
+                                existing_library['description'] = input(f"请输入库描述（{package_name}）：")
+                            elif field == 'package_names':
+                                package_names_input = input(f"请输入库包名（{package_name}）（以逗号分隔）：")
+                                existing_library['package_names'] = package_names_input.split(',')
+
+                        # 更新库信息到CSV文件
+                        add_library(existing_library)
+
+        print("现有数据修复完成并保存到 sdk_libraries.csv 文件中。")
+        break
+
+    elif input_option.lower() == "q":
+        print("程序结束。")
+        break
